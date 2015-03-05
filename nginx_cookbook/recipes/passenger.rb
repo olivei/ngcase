6d87520ad40ca34452312ec0bb99af732e64f34f
oliveirf <fransol@sapo.pt>
2015-03-05 18:52:05 -0500

    cs

--- nginx_cookbook/recipes/passenger.rb
+++ nginx_cookbook/recipes/passenger.rb
@@ -105,7 +105,7 @@ template "#{node[:nginx][:dir]}/stack.conf" do
   mode 00644
   variables (
     lazy {
-     {passenger_root: `bash -c "passenger-config --root"`.strip},
+     {passenger_root: `bash -c "passenger-config --root"`.strip,
     passenger_max_pool_size: node[:passenger][:max_pool_size],
     passenger_pool_idle_time: node[:passenger][:pool_idle_time],
     passenger_default_user: node[:passenger][:default_user],
@@ -116,6 +116,7 @@ template "#{node[:nginx][:dir]}/stack.conf" do
     proxy_buffer_size: node[:proxy][:buffer_size],
     proxy_buffers: node[:proxy][:buffers],
     proxy_busy_buffers_size: node[:proxy][:busy_buffers_size]
+  }
   }
   )
 end
